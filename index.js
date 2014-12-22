e942bfa5ebf7eb0798c62122157c13ef160fa35c
jigsaw <jigsaw@live.jp>
2014-12-22 18:07:20 +0900

    Update 2014-12-22T09:07:17.629Z

---
+++ index.js
@@ -0,0 +1,28 @@
+(function() {
+  var convert, renderer;
+
+  renderer = new marked.Renderer();
+
+  renderer.code = function(code, lang) {
+    return "<pre class=\"lang:" + (lang != null ? lang : 'default') + " decode:true\">" + code + "</pre>\n";
+  };
+
+  renderer.heading = function(text, level) {
+    return "<h" + level + ">" + text + "</h" + level + ">\n";
+  };
+
+  convert = function(event) {
+    marked.setOptions({
+      renderer: renderer
+    });
+    document.getElementById('dummy').innerHTML = marked(document.getElementById('md').value);
+    return document.getElementById('wp').value = document.getElementById('dummy').innerHTML.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+  };
+
+  document.getElementById('convert').addEventListener('click', convert);
+
+  document.getElementById('wp').addEventListener('focus', function(event) {
+    return this.select();
+  });
+
+}).call(this);
